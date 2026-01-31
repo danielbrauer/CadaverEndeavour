59100028bbfec1f058a943c9982eda3137da6214
Leon Matthes <leon@matthes.biz>
2026-01-31 12:18:52 +0100

    More project setup

--- scenes/attachment_point.gd
+++ scenes/attachment_point.gd
@@ -1,6 +1,7 @@
 extends Area2D
 
 
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
@@ -8,7 +9,9 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
+	get_tree().get_first_node_in_group("cadaver")
 	pass
 
 func _on_area_entered(area: Area2D) -> void:
+	
 	pass # Replace with function body.
