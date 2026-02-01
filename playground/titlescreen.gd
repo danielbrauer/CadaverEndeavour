81cf6e2641c4b39a3680f00021172cdd0fdf1301
Erik Meseck <git@meseck.dev>
2026-02-01 11:20:15 +0100

    Switch to 'press space to start' in titlescreen

--- playground/titlescreen.gd
+++ playground/titlescreen.gd
@@ -1,22 +1,24 @@
 extends Control
 
 func on_finished() -> void:
+	print("Triggerd!")
 	AppStateManager.currentState = AppStateManager.States.INTRO
-
-func _on_start_game_pressed() -> void:
-	$AudioStreamPlayer.play()
 	
-	var shake_duration = 0.05
-	var shake_rotation = 0.15
-	var shake_amount: = 20.0
+func _input(_event):
+	if Input.is_action_just_released("start"):
+		$AudioStreamPlayer.play()
 	
-	var tween = get_tree().create_tween()
-	
-	for i in shake_amount:
-		tween.tween_property($Telephone, "rotation", shake_rotation, shake_duration)
-		tween.tween_property($Telephone, "rotation", shake_rotation * -1, shake_duration)
+		var shake_duration = 0.05
+		var shake_rotation = 0.15
+		var shake_amount: = 20.0
 		
-		if (i + 1 == shake_amount):
-			tween.tween_property($Telephone, "rotation", 0, shake_duration)
+		var tween = get_tree().create_tween()
+		
+		for i in shake_amount:
+			tween.tween_property($Telephone, "rotation", shake_rotation, shake_duration)
+			tween.tween_property($Telephone, "rotation", shake_rotation * -1, shake_duration)
+			
+			if (i + 1 == shake_amount):
+				tween.tween_property($Telephone, "rotation", 0, shake_duration)
 
-	$AudioStreamPlayer.finished.connect(on_finished)
+		$AudioStreamPlayer.finished.connect(on_finished)
